101b4a01a4e53d06e89db81702af760076a8b957
Mark C <root@DESKTOP-PRSRRPF.localdomain>
2020-12-13 10:39:23 -0500

    Added new tables

--- database/schema.sql
+++ database/schema.sql
@@ -1,10 +1,25 @@
 DROP TABLE IF EXISTS tReddit_Comments;
+DROP TABLE IF EXISTS tCourses;
+DROP TABLE IF EXISTS tReports;
+
 CREATE TABLE tReddit_Comments (
-	course			VARCHAR(100)
+	commentId		SERIAL
+	,courseId		VARCHAR(100)
 	,professor		VARCHAR(100)
-	,sentiment_rating	FLOAT --subject to discussion
+	,sentiment_rating	FLOAT
 	,content		TEXT
 	,link			VARCHAR(500)
 	,upvotes		INT
-	,UNIQUE (course,professor,link)
+	,UNIQUE (courseId,professor,link)
 );
+
+CREATE TABLE tCourses (
+	courseId		VARCHAR(100)
+    ,courseName 	VARCHAR(200)
+);
+
+CREATE TABLE tReports (
+	ip_address	VARCHAR(100)
+	,commentId	INT
+	,reportContent  TEXT
+)
